0e0d7d2068931dc5d7355b30db06a4e1234b9d58
peyton <peyton.curr@gmail.com>
2023-03-21 16:39:41 -0600

    Stopped Working for some reason

--- server/controllers/HousesController.js
+++ server/controllers/HousesController.js
@@ -9,6 +9,10 @@ export class HousesController extends BaseController {
         super(`api/houses`)
         this.router
             .get(``, this.getHouses)
+            .post(``, this.createHouse)
+            .get(`/:houseId`, this.getHouseById)
+            .put(`/:houseId`, this.editHouse)
+            .delete(`/:houseId`, this.deleteHouse)
 
     }
     async getHouses(req, res, next) {
@@ -21,4 +25,44 @@ export class HousesController extends BaseController {
         }
     }
 
+    async createHouse(req, res, next) {
+        try {
+            const houseData = req.body
+            const house = await housesService.createHouse(houseData);
+            res.send(house)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getHouseById(req, res, next) {
+        try {
+            const houseId = req.params.houseId
+            const house = await housesService.getHouseById(houseId);
+            res.send(house)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async deleteHouse(req, res, next) {
+        try {
+            const houseId = req.params.houseId
+            await housesService.deleteHouse(houseId);
+            res.send(`The house ID: ${houseId} was Deleted`)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async editHouse(req, res, next) {
+        try {
+            const houseEdits = req.body
+            const houseId = req.params.houseId
+            const house = await housesService.editHouse(houseEdits, houseId);
+            res.send(house)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
--- server/services/HousesService.js
+++ server/services/HousesService.js
@@ -1,11 +1,46 @@
+// @ts-nocheck
 import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js"
 import { dbContext } from "../db/DbContext.js";
 
 
 class HousesService {
+
     async getHouses(query) {
         const houses = await dbContext.Houses.find(query)
         return houses
+    }
+
+    async createHouse(houseData) {
+        if (!houseData.price || !houseData.beds || !houseData.baths || !houseData.sqFt || !houseData.address) {
+            new BadRequest(`No house Data was Found`)
+        }
+        const house = await dbContext.Houses.create(houseData)
+        return house
+    }
+
+    async getHouseById(houseId) {
+        const house = await dbContext.Houses.findById(houseId)
+        if (!houseId) {
+            new BadRequest(`No house with that id was found`)
+        }
+        return house
+    }
+
+    async deleteHouse(houseId) {
+        await dbContext.Houses.findByIdAndDelete(houseId)
+        return
+    }
+
+    async editHouse(houseEdits, houseId) {
+        const originalHouse = await this.getHouseById(houseId);
+        originalHouse.price = houseEdits.price || originalHouse.price
+        // NOTE these do the same thing
+        originalHouse.beds = houseEdits.beds ? houseEdits.beds : originalHouse?.beds
+        originalHouse.baths = houseEdits.baths || originalHouse.baths
+        originalHouse.sqRt = houseEdits.sqRt || originalHouse.sqRt
+        originalHouse.address = houseEdits.address || originalHouse.address
+        await originalHouse.save();
+        return originalHouse
     }
 
 }
